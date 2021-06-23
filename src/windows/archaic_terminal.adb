a71cc9ad79682017b20e0e2b9ce0e3e08c874229
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-23 17:57:44 -0400

    Bring in input and output console mode enums.

--- src/windows/archaic_terminal.adb
+++ src/windows/archaic_terminal.adb
@@ -46,6 +46,57 @@ package body Archaic_Terminal is
         pragma Import (Stdcall, SetConsoleCP, "SetConsoleCP");
         pragma Import (Stdcall, GetConsoleOutputCP, "GetConsoleOutputCP");
         pragma Import (Stdcall, SetConsoleOutputCP, "SetConsoleOutputCP");
+
+        --!pp off
+        type Input_Console_Modes is
+            (ENABLE_PROCESSED_INPUT,
+             ENABLE_LINE_INPUT,
+             ENABLE_ECHO_INPUT,
+             ENABLE_WINDOW_INPUT,
+             ENABLE_MOUSE_INPUT,
+             ENABLE_INSERT_MODE,
+             ENABLE_QUICK_EDIT_MODE,
+             ENABLE_EXTENDED_FLAGS,
+             ENABLE_AUTO_POSITION,
+             ENABLE_VIRTUAL_TERMINAL_INPUT);
+
+        for Input_Console_Modes use
+            (ENABLE_PROCESSED_INPUT        => 16#0001#,
+             ENABLE_LINE_INPUT             => 16#0002#,
+             ENABLE_ECHO_INPUT             => 16#0004#,
+             ENABLE_WINDOW_INPUT           => 16#0008#,
+             ENABLE_MOUSE_INPUT            => 16#0010#,
+             ENABLE_INSERT_MODE            => 16#0020#,
+             ENABLE_QUICK_EDIT_MODE        => 16#0040#,
+             ENABLE_EXTENDED_FLAGS         => 16#0080#,
+             ENABLE_AUTO_POSITION          => 16#0100#,
+             ENABLE_VIRTUAL_TERMINAL_INPUT => 16#0200#);
+
+        type Output_Console_Modes is
+            (ENABLE_PROCESSED_OUTPUT,
+             ENABLE_WRAP_AT_EOL_OUTPUT,
+             ENABLE_VIRTUAL_TERMINAL_PROCESSING,
+             DISABLE_NEWLINE_AUTO_RETURN,
+             ENABLE_LVB_GRID_WORLDWIDE);
+
+        for Output_Console_Modes use
+            (ENABLE_PROCESSED_OUTPUT            => 16#0001#,
+             ENABLE_WRAP_AT_EOL_OUTPUT          => 16#0002#,
+             ENABLE_VIRTUAL_TERMINAL_PROCESSING => 16#0004#,
+             DISABLE_NEWLINE_AUTO_RETURN        => 16#0008#,
+             ENABLE_LVB_GRID_WORLDWIDE          => 16#0010#);
+        --!pp on
+
+        pragma Warnings (Off, "bits of *unused");
+        type Input_Console_Mode is array (Input_Console_Modes) of Boolean with
+            Pack,
+            Size => 32;
+        type Output_Console_Mode is array (Output_Console_Modes) of Boolean with
+            Pack,
+            Size => 32;
+        pragma Warnings (On, "bits of *unused");
+
+        pragma Unreferenced (Input_Console_Mode, Output_Console_Mode);
     end Windows_Bindings;
     ---------------------------------------------------------------------------
     --
@@ -54,7 +105,6 @@ package body Archaic_Terminal is
     use type Win.BOOL;
     use type Win.HANDLE;
     ---------------------------------------------------------------------------
-
 
     ---------------------------------------------------------------------------
     -- Original settings
@@ -70,7 +120,7 @@ package body Archaic_Terminal is
     ---------------------------------------------------------------------------
     -- The triad of I/O streams.
     ---------------------------------------------------------------------------
-    Std_Input, Std_Output, Std_Error                : IOStream;
+    Std_Input, Std_Output, Std_Error : IOStream;
 
     function Gather (Stream : in out IOStream) return Boolean is
     begin
@@ -79,7 +129,7 @@ package body Archaic_Terminal is
 
     function Enable_UTF8 return Boolean is
     begin
-        return Win.SetConsoleCP(Win.CP_UTF8) /= 0 and then Win.SetConsoleOutputCP(Win.CP_UTF8) /= 0;
+        return Win.SetConsoleCP (Win.CP_UTF8) /= 0 and then Win.SetConsoleOutputCP (Win.CP_UTF8) /= 0;
     end Enable_UTF8;
 
     function Init return Boolean is
