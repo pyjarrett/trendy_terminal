64567934d84e8ce55ef4e3529bd9a91ae0645bfe
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-23 22:23:57 -0400

    Add isatty and fileno functions.

--- tests/src/archaic_terminal_test.adb
+++ tests/src/archaic_terminal_test.adb
@@ -19,36 +19,38 @@ begin
         Ada.Text_IO.Put_Line ("Archaic terminal failed to initialized.");
     end if;
 
-    Archaic_Terminal.Set(Archaic_Terminal.Echo, False);
-    Archaic_Terminal.Set(Archaic_Terminal.Line_Input, False);
-    Archaic_Terminal.Set(Archaic_Terminal.Escape_Sequences, True);
+    if False then
+        Archaic_Terminal.Set (Archaic_Terminal.Echo, False);
+        Archaic_Terminal.Set (Archaic_Terminal.Line_Input, False);
+        Archaic_Terminal.Set (Archaic_Terminal.Escape_Sequences, True);
 
-    if Show_Progress_Test then
-        for Width of Bar_Widths loop
-            for I in Percentage'(0) .. 100 loop
-                delay 0.005;
-                Ada.Text_IO.Put (Get_Bar (I, Width));
+        if Show_Progress_Test then
+            for Width of Bar_Widths loop
+                for I in Percentage'(0) .. 100 loop
+                    delay 0.005;
+                    Ada.Text_IO.Put (Get_Bar (I, Width));
+                end loop;
+                Ada.Text_IO.New_Line;
             end loop;
-            Ada.Text_IO.New_Line;
-        end loop;
 
-        for I in 1 .. 100 loop
-            delay 0.005;
-            Tick (S);
-            Ada.Text_IO.Put (Value (S));
-        end loop;
+            for I in 1 .. 100 loop
+                delay 0.005;
+                Tick (S);
+                Ada.Text_IO.Put (Value (S));
+            end loop;
+        end if;
+
+        declare
+            Last : Character;
+        begin
+            loop
+                Ada.Text_IO.Get (Last);
+                Ada.Text_IO.Put_Line ("CHAR : " & Last'Image);
+                exit when Last = Ada.Characters.Latin_1.CR;
+                exit when Last = Ada.Characters.Latin_1.LF;
+            end loop;
+        end;
     end if;
-
-    declare
-        Last : Character;
-    begin
-        loop
-            Ada.Text_IO.Get (Last);
-            Ada.Text_IO.Put_Line ("CHAR : " & Last'Image);
-            exit when Last = Ada.Characters.Latin_1.CR;
-            exit when Last = Ada.Characters.Latin_1.LF;
-        end loop;
-    end;
 
     Archaic_Terminal.Shutdown;
 end Archaic_Terminal_Test;
