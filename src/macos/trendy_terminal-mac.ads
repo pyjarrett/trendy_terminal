b37259f18e9bece10d35500a9d56513de3a2b79e
Paul Jarrett <jarrett.paul.young@gmail.com>
2022-04-18 20:40:04 -0400

    Version bump due to preliminary Mac support.

---
+++ src/macos/trendy_terminal-mac.ads
@@ -0,0 +1,136 @@
+-------------------------------------------------------------------------------
+-- Copyright 2021, The Trendy Terminal Developers (see AUTHORS file)
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-------------------------------------------------------------------------------
+
+with Interfaces.C.Strings;
+with System;
+
+package Trendy_Terminal.Mac is
+
+    ---------------------------------------------------------------------------
+    -- Interfacing with C
+    ---------------------------------------------------------------------------
+    -- Crash course in how this works.
+    -- https://en.wikibooks.org/wiki/Ada_Programming/Types/access#Access_vs._System.Address
+    type BOOL is new Interfaces.C.int;
+    type FD is new Interfaces.C.int;
+    type FILE_Ptr is new System.Address;
+
+    function fileno (Stream : FILE_Ptr) return FD with
+        Import     => True,
+        Convention => C;
+
+    function isatty (File_Descriptor : FD) return BOOL with
+        Import     => True,
+        Convention => C;
+
+    stdin  : aliased FILE_Ptr;
+    stdout : aliased FILE_Ptr;
+    stderr : aliased FILE_Ptr;
+
+    pragma Import (C, stdin, "__stdinp");
+    pragma Import (C, stdout, "__stdoutp");
+    pragma Import (C, stderr, "__stderrp");
+
+    NCCS : constant := 32;
+
+    type tcflag_t is new Interfaces.C.unsigned_long;
+    type cc_t is new Interfaces.C.unsigned_char;
+    type speed_t is new Interfaces.C.unsigned_long;
+    type cc_array is array (Natural range 0 .. NCCS - 1) of cc_t;
+
+    --!pp off
+    type c_lflag_t is (
+       ECHOKE     ,
+       ECHOE      ,
+       ECHOK      ,
+       ECHO       ,
+       ECHONL     ,
+       ECHOPRT    ,
+       ECHOCTL    ,
+       ISIG       ,
+       ICANON     ,
+       ALTWERASE  ,
+       IEXTEN     ,
+       EXTPROC    ,
+       Unused1    ,
+       Unused2    ,
+       TOSTOP     ,
+       FLUSHO     ,
+       Unused3    ,
+       NOKERNINFO ,
+       PENDIN     ,
+       NOFLSH);
+                           
+    for c_lflag_t use
+      (
+       ECHOKE     => 16#00000001#,
+       ECHOE      => 16#00000002#,
+       ECHOK      => 16#00000004#,
+       ECHO       => 16#00000008#,
+       ECHONL     => 16#00000010#,
+       ECHOPRT    => 16#00000020#,
+       ECHOCTL    => 16#00000040#,
+       ISIG       => 16#00000080#,
+       ICANON     => 16#00000100#,
+       ALTWERASE  => 16#00000200#,
+       IEXTEN     => 16#00000400#,
+       EXTPROC    => 16#00000800#,
+       Unused1    => 16#00100000#,
+       Unused2    => 16#00200000#,
+       TOSTOP     => 16#00400000#,
+       FLUSHO     => 16#00800000#,
+       Unused3    => 16#01000000#,
+       NOKERNINFO => 16#02000000#,
+       PENDIN     => 16#20000000#,
+       NOFLSH     => 16#80000000#
+      );
+    --!pp on
+
+    pragma Warnings (Off, "bits of *unused");
+    type Local_Flags is array (c_lflag_t) of Boolean with
+        Pack,
+        Size => 32;
+    pragma Warnings (On, "bits of *unused");
+
+    type Termios is record
+        c_iflag  : tcflag_t;
+        c_oflag  : tcflag_t;
+        c_cflag  : tcflag_t;
+        c_lflag  : Local_Flags;
+        c_line   : cc_t;
+        c_cc     : cc_array;
+        c_ispeed : speed_t;
+        c_ospeed : speed_t;
+    end record with
+        Convention => C;
+
+    function tcgetattr (File_Descriptor : FD; Terminal : System.Address) return BOOL with
+        Import     => True,
+        Convention => C;
+
+    type Application_Time is
+        (TCSANOW,   -- immediate effect
+         TCSADRAIN, -- after all output written
+         TCSAFLUSH  -- like drain, except input received as well
+    );
+    for Application_Time use (TCSANOW => 0, TCSADRAIN => 1, TCSAFLUSH => 2);
+
+    function tcsetattr
+        (File_Descriptor : FD; Effect_Time : Application_Time; Terminal : System.Address) return BOOL with
+        Import     => True,
+        Convention => C;
+
+end Trendy_Terminal.Mac;
---
+++ src/macos/trendy_terminal-platform.adb
@@ -0,0 +1,176 @@
+-------------------------------------------------------------------------------
+-- Copyright 2021, The Trendy Terminal Developers (see AUTHORS file)
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+with Ada.Unchecked_Conversion;
+
+with Interfaces.C.Strings;
+
+with System;
+
+with Trendy_Terminal.Mac;
+with Trendy_Terminal.Maps;
+
+package body Trendy_Terminal.Platform is
+    use type Mac.BOOL;
+
+    ---------------------------------------------------------------------------
+    --
+    ---------------------------------------------------------------------------
+    type IOStream is record
+        File            : Mac.FILE_Ptr;
+        File_Descriptor : Mac.FD;
+        Settings        : aliased Mac.Termios;
+    end record;
+
+    ---------------------------------------------------------------------------
+    -- Original settings
+    ---------------------------------------------------------------------------
+    Original_Input_Setting, Original_Output_Setting, Original_Error_Setting :
+    Mac.Termios;
+
+    ---------------------------------------------------------------------------
+    -- The triad of I/O streams.
+    ---------------------------------------------------------------------------
+    Std_Input, Std_Output, Std_Error : IOStream;
+
+    function Load_Terminal (File_Descriptor : Mac.FD; Terminal : not null access
+        Mac.Termios) return Boolean is
+    begin
+        Ada.Text_IO.Put_Line ("Loading terminal "
+            & Mac.FD'Image (File_Descriptor));
+        if Mac.isatty (File_Descriptor) = 0 then
+            Ada.Text_IO.Put_Line ("Unable to load file descriptor for terminal.");
+            return False;
+        end if;
+        if Mac.tcgetattr (File_Descriptor, Terminal.all'Address) /= 0 then
+            Ada.Text_IO.Put_Line ("Unable to get terminal attributes.");
+            return False;
+        end if;
+        return True;
+    end Load_Terminal;
+
+    function Make_Handle (File_Descriptor : Mac.FILE_Ptr; Handle : out IOStream) return Boolean is
+    begin
+        Handle.File := File_Descriptor;
+        Handle.File_Descriptor := Mac.fileno (Handle.File);
+        return Load_Terminal (Handle.File_Descriptor, Handle.Settings'Unchecked_Access);
+    end Make_Handle;
+
+    procedure Require_Settings_Change (Stream : IOStream) with
+        Pre => Mac.isatty (Stream.File_Descriptor) /= 0
+    is
+    begin
+        if Mac.tcsetattr (Stream.File_Descriptor, Mac.TCSANOW, Stream.Settings'Address) /= 0 then
+            Ada.Text_IO.Put_Line ("Unable to change settings.");
+        end if;
+    end Require_Settings_Change;
+
+    function Init return Boolean is
+        -- Initializes and captures the original settings for the terminal so they can
+        -- be restored when the system is shutdown.
+    begin
+        if not Make_Handle (Mac.stdin, Std_Input) or else not Make_Handle (Mac.stdout, Std_Output)
+            or else not Make_Handle (Mac.stderr, Std_Error) then
+            Ada.Text_IO.Put_Line ("Unable to get standard stream handles.");
+        end if;
+
+        -- Save the startup settings.
+        Original_Input_Setting  := Std_Input.Settings;
+        Original_Output_Setting := Std_Output.Settings;
+        Original_Error_Setting  := Std_Error.Settings;
+        return True;
+    end Init;
+
+    procedure Shutdown is
+    begin
+        Std_Input.Settings  := Original_Input_Setting;
+        Std_Output.Settings := Original_Output_Setting;
+        Std_Error.Settings  := Original_Error_Setting;
+
+        Require_Settings_Change (Std_Input);
+        Require_Settings_Change (Std_Output);
+        Require_Settings_Change (Std_Error);
+    end Shutdown;
+
+    procedure Set (Setting : Platform.Input_Setting; Enabled : Boolean) is
+    begin
+        Ada.Text_IO.Put_Line ("Setting flag");
+        case Setting is
+            when Platform.Echo =>
+                Std_Input.Settings.c_lflag (Mac.ECHO) := Enabled;
+            when Platform.Line_Input =>
+                Std_Input.Settings.c_lflag (Mac.ICANON) := Enabled;
+            when Platform.Signals_As_Input =>
+                Std_Input.Settings.c_lflag (Mac.ISIG) := not Enabled;
+        end case;
+        Require_Settings_Change (Std_Input);
+    end Set;
+
+    procedure Set (Setting : Output_Setting; Enabled : Boolean) is
+    begin
+        pragma Unreferenced (Enabled);
+        case Setting is
+            when Escape_Sequences =>
+                null;
+                -- nothing to do here
+        end case;
+    end Set;
+
+    procedure Put (C : Character) renames Ada.Text_IO.Put;
+    procedure Put (S : String) renames Ada.Text_IO.Put;
+
+    type VOIDP is new Interfaces.C.Strings.chars_ptr;
+    function Read (File_Descriptor : Mac.FD; Buffer : VOIDP; Buffer_Size : Natural) return Integer
+        with Import     => True,
+             Convention => C;
+
+    -- Gets an entire input line from one keypress.  E.g. all the characters
+    -- received for a controlling keypress, such as an arrow key.
+    function Get_Input return String is
+        Buffer_Size  : constant := 512;
+        Buffer       : aliased Interfaces.C.char_array := (1 .. Interfaces.C.size_t(Buffer_Size) => Interfaces.C.nul);
+        Chars_Read   : Integer;
+        use all type Interfaces.C.size_t;
+    begin
+        Chars_Read := Read (Mac.fileno (Std_Input.File),
+            VOIDP (Interfaces.C.Strings.To_Chars_Ptr (Buffer'Unchecked_Access)),
+            Buffer_Size);
+        if Chars_Read > 0 then
+            return Interfaces.C.To_Ada(Buffer(1 .. Interfaces.C.size_t(Chars_Read) + 1));
+        else
+            return "";
+        end if;
+    end Get_Input;
+
+    function End_Of_Line return String is
+    begin
+        return (1 => Maps.Characters.LF);
+    end End_Of_Line;
+
+    procedure Print_Configuration is
+        function To_Integer is new Ada.Unchecked_Conversion (Mac.Local_Flags, Integer);
+    begin
+        Ada.Text_IO.Put_Line ("Original Input Mode:  " & To_Integer (Original_Input_Setting.c_lflag)'Image);
+        Ada.Text_IO.Put_Line ("Original Output Mode: " & To_Integer (Original_Output_Setting.c_lflag)'Image);
+        Ada.Text_IO.Put_Line ("Original Error Mode:  " & To_Integer (Original_Error_Setting.c_lflag)'Image);
+
+        Ada.Text_IO.Put_Line ("Input Mode:  " & To_Integer (Std_Input.Settings.c_lflag)'Image);
+        Ada.Text_IO.Put_Line ("Output Mode: " & To_Integer (Std_Output.Settings.c_lflag)'Image);
+        Ada.Text_IO.Put_Line ("Error Mode:  " & To_Integer (Std_Error.Settings.c_lflag)'Image);
+    end Print_Configuration;
+
+end Trendy_Terminal.Platform;
